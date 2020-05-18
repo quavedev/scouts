77ea3b47a74a9a81e5fe3ced4eeda943bc0ee794
filipenevola <filipenevola@gmail.com>
2020-05-18 07:43:36 -0400

    Moving to packages

--- packages/custom-type-date-time/DateTimeResolver.js
+++
@@ -1,8 +0,0 @@
-import { GraphQLScalarType } from 'graphql';
-import { CustomTypes } from 'meteor/quave:collections/CustomType';
-
-import { DateTimeType } from './DateTimeType';
-
-export const DateTimeResolver = {
-  DateTime: new GraphQLScalarType(CustomTypes.scalarAndEjson(DateTimeType)),
-};
